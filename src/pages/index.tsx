73a926d3a249b562402a50168ce83a535f627d5f
Alexandre Feio <alefeio@gmail.com>
2025-09-17 02:43:36 -0300

    google tag e analytics vercel - 8

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -130,19 +130,6 @@ export default function Home({ banners, menu, testimonials, faqs, destinos, gall
     return (
         <>
             <Head>
-                {/* Google Tag Manager */}
-                <Script
-                    id="gtm-script"
-                    strategy="afterInteractive"
-                    dangerouslySetInnerHTML={{
-                        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                        })(window,document,'script','dataLayer','GTM-TZDNZCCL');`
-                    }}
-                />
-                
                 <title>D' Hages Turismo | Agência de Viagens em Belém - Pacotes e Destinos</title>
                 <meta
                     name="description"
@@ -205,16 +192,6 @@ export default function Home({ banners, menu, testimonials, faqs, destinos, gall
                     `}
                 </script>
             </Head>
-
-            {/* Google Tag Manager (noscript) */}
-            <noscript>
-                <iframe
-                    src="https://www.googletagmanager.com/ns.html?id=GTM-TZDNZCCL"
-                    height="0"
-                    width="0"
-                    style={{ display: 'none', visibility: 'hidden' }}
-                ></iframe>
-            </noscript>
 
             <div className="min-h-screen">
                 <Analytics />
