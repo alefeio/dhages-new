f32fe9f21c6efbbe1c2f87adcad7af8cb2131bfe
Alexandre Feio <alefeio@gmail.com>
2025-11-12 11:07:32 -0300

    cnpj

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -131,7 +131,8 @@ const Footer = ({ menuData }: MenuProps) => {
                     {/* Direitos Autorais e Créditos */}
                     <div className="text-center md:text-left border-t border-gray-700 mt-8 pt-6">
                         <p className="text-sm text-white">
-                            © 2025 D' Hages Turismo. Todos os direitos reservados.
+                            © 2025 D' Hages Turismo. Todos os direitos reservados.<br />
+                            CNPJ: 23.167.053/0001-05
                         </p>
                     </div>
                 </div>
